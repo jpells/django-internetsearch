357c3f3e8063d4d7257450e1732814245db7e3bb
James.Pells <James.Pells@6325216b-dc37-0410-aacf-b5ded037b3b3>
2008-05-29 14:06:05 +0000

    relocate templates

--- internetsearch/views.py
+++ internetsearch/views.py
@@ -12,6 +12,6 @@ def search(request, term = None, results_number = 10):
         if request.POST:
             term = request.POST['term']
         result = google(term + site_search_term, results_number)
-        return render_to_response('search/search.html', {'term': term, 'result': result, 'results_number': results_number}, context_instance=RequestContext(request))
+        return render_to_response('internetsearch/search.html', {'term': term, 'result': result, 'results_number': results_number}, context_instance=RequestContext(request))
     else:
-    	return render_to_response('search/search.html', context_instance=RequestContext(request))
+    	return render_to_response('internetsearch/search.html', context_instance=RequestContext(request))
