b14606ac7bdad2fb5753ab31940d2f5c49dc8d11
James.Pells <James.Pells@6325216b-dc37-0410-aacf-b5ded037b3b3>
2008-06-12 19:20:29 +0000

    refactoring Site.objects.get_current

--- internetsearch/views.py
+++ internetsearch/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render_to_response
 from internetsearch.web_search import google
 from django.template import RequestContext
 from django.contrib.sites.models import Site
-from django.conf import settings
 
 def search(request, term = None, results_number = 10):
     if request.POST or term:
         results_number = int(results_number)
-        current_site = Site.objects.get(id=settings.SITE_ID)
-        site_search_term = " site:%s" % current_site.domain
+        site_search_term = " site:%s" % Site.objects.get_current().domain
         if request.POST:
             term = request.POST['term']
         result = google(term + site_search_term, results_number)
