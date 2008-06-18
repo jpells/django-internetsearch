30e3b532096b8d2fd6a541f864a47d447750c9ac
James.Pells <James.Pells@6325216b-dc37-0410-aacf-b5ded037b3b3>
2008-06-18 17:55:53 +0000

    use named urls to stay DRY

--- internetsearch/urls.py
+++ internetsearch/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
+from internetsearch import views
 
-urlpatterns = patterns('internetsearch.views',
-    (r'^$', 'search',),
-    (r'^(?P<term>[-\w]+)/$', 'search',),
-    (r'^(?P<term>[-\w]+)/(?P<results_number>\d+)/$', 'search',),
+urlpatterns = patterns('',
+    url(r'^$', views.search, name="internetsearch_search"),
+    url(r'^(?P<term>[-\w]+)/$', views.search, name="internetsearch_search_term"),
+    url(r'^(?P<term>[-\w]+)/(?P<results_number>\d+)/$', views.search, name="internetsearch_search_term_results"),
 )
